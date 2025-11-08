f7a2a11e47bbbd8d303d3fb26b082f6163b9ad67
marcosmatica <marcosgt_rodrigues@hotmail.com>
2025-11-07 23:46:58 -0300

    att

--- comissao_disciplinar/napne/views.py
+++ comissao_disciplinar/napne/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
+from django.utils import timezone
 from django.db.models import Q
 from .models import (
     FichaEstudanteNAPNE, AtendimentoNAPNE, TipoAtendimentoNAPNE,
